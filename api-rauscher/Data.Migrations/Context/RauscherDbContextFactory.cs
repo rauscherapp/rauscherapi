e770085a5036b29da485c516878ef5cbc262a333
Vinicius Espuri Barboza <vinicius.barboza@totvspartners.com.br>
2025-07-01 19:41:49 -0300

    AppSettings e StringConnections

--- api-rauscher/Data.Migrations/Context/RauscherDbContextFactory.cs
+++ api-rauscher/Data.Migrations/Context/RauscherDbContextFactory.cs
@@ -14,14 +14,15 @@ namespace Data.Migrations.Context
             var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "Data");
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(basePath)
-                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile("appsettings.json", optional: false)
                 .Build();
 
+            var connectionString = "Server=tcp:rauscher-db.database.windows.net,1433;Initial Catalog=db_rauscher;Persist Security Info=False;User ID=adminsql;Password=Rauscher@2025*;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
             var optionsBuilder = new DbContextOptionsBuilder<RauscherDbContext>();
-            optionsBuilder.UseSqlServer(
-                "Server=localhost,1433;Database=RauscherApp-Dev;User Id=sa; Password=!@Lexos_Hub-Admin12;TrustServerCertificate=true;",
-                b => b.MigrationsAssembly("Data.Migrations")
-                );
+            optionsBuilder.UseSqlServer(connectionString,
+                b => b.MigrationsAssembly("Data.Migrations"));
+
             return new RauscherDbContext(optionsBuilder.Options);
         }
     }
