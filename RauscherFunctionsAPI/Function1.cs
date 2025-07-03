ed55b87dc33063869627ac653820b9e7ce3751df
Vinicius Espuri Barboza <vinicius.barboza@totvspartners.com.br>
2025-07-03 19:22:41 -0300

    Function Test

--- RauscherFunctionsAPI/Function1.cs
+++ RauscherFunctionsAPI/Function1.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -11,24 +12,12 @@ namespace RauscherFunctionsAPI
 {
   public static class Function1
   {
-    [FunctionName("Function1")]
+    [FunctionName("Ping")]
     public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-        ILogger log)
-    {
-      log.LogInformation("C# HTTP trigger function processed a request.");
-
-      string name = req.Query["name"];
-
-      string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-      dynamic data = JsonConvert.DeserializeObject(requestBody);
-      name = name ?? data?.name;
-
-      string responseMessage = string.IsNullOrEmpty(name)
-          ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-          : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-      return new OkObjectResult(responseMessage);
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")] HttpRequest req,
+    ILogger log)
+        {
+            return new OkObjectResult(new { status = "ok", time = DateTime.UtcNow });
+        }
     }
-  }
 }
