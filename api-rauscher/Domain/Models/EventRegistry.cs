a61a8a3f29f5a2714001d43706f752f8b459d34e
Vinicius Espuri Barboza <vinicius.barboza@totvspartners.com.br>
2025-07-09 18:12:09 -0300

    Ajuste EventLink

--- api-rauscher/Domain/Models/EventRegistry.cs
+++ api-rauscher/Domain/Models/EventRegistry.cs
@@ -2,34 +2,34 @@ using System;
 
 namespace Domain.Models
 {
-	public class EventRegistry
-	{
-    public EventRegistry(
-    Guid id,
-    string eventName,
-    string eventDescription,
-    string eventType,
-    DateTime eventDate,
-    string eventLocation,
-    string eventLink,
-    bool published)
+    public class EventRegistry
     {
-      Id = id;
-      EventName = eventName;
-      EventDescription = eventDescription;
-      EventType = eventType;
-      EventDate = eventDate;
-      EventLocation = eventLocation;
-      EventLink = eventLink;
-      Published = published;
+        public EventRegistry(
+        Guid id,
+        string eventName,
+        string eventDescription,
+        string eventType,
+        DateTime eventDate,
+        string eventLocation,
+        string eventLink,
+        bool published)
+        {
+            Id = id;
+            EventName = eventName;
+            EventDescription = eventDescription;
+            EventType = eventType;
+            EventDate = eventDate;
+            EventLocation = eventLocation;
+            EventLink = (!string.IsNullOrEmpty(eventLink)) ? eventLink : null;
+            Published = published;
+        }
+        public Guid Id { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public string EventType { get; set; }
+        public DateTime EventDate { get; set; }
+        public string EventLocation { get; set; }
+        public string EventLink { get; set; }
+        public bool Published { get; set; }
     }
-    public Guid Id { get; set; }
-		public string EventName { get; set; }
-    public string EventDescription { get; set; }
-		public string EventType { get; set; }
-		public DateTime EventDate { get; set; }
-		public string EventLocation { get; set; }
-		public string EventLink { get; set; }
-		public bool Published { get; set; }
-	}
 }
